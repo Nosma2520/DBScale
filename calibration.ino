931c6d9e27cc8e490ddff59caca6f4454faf8bc7
Nosma2520 <nexiattiogbe@gmail.com>
2022-07-03 11:53:45 -0400

    Created sketch to convert applied force on the load cell to voltage

--- calibration.ino
+++ calibration.ino
@@ -1,1 +1,21 @@
+#include <HX711.h>
 
+const int DOUT = 4;
+const int CLK = 5;
+HX711 scale;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  scale.begin(DOUT, CLK); 
+}
+
+void loop() {
+  // Oversampling
+  long adc = scale.read_average(25);
+  //(2^24)-1
+  // 24 bit ADC
+  double voltage = 5.0*adc/16777215;
+  Serial.print("Voltage:\t\t");
+  Serial.println(voltage);
+}
